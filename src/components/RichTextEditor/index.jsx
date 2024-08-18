67e7d5a01daa5249a7fac0719d45fa5f42b3a037
Code A Program <75136330+Sridhar-C-25@users.noreply.github.com>
2024-08-18 16:03:21 +0530

    Update index.jsx

--- src/components/RichTextEditor/index.jsx
+++ src/components/RichTextEditor/index.jsx
@@ -6,6 +6,8 @@ import ToolBar from "./ToolBar";
 import Heading from "@tiptap/extension-heading";
 import Highlight from "@tiptap/extension-highlight";
 import Image from "@tiptap/extension-image";
+import BulletList from "@tiptap/extension-bullet-list";
+import OrderedList from "@tiptap/extension-ordered-list";
 import ImageResize from "tiptap-extension-resize-image";
 
 export default function RichTextEditor({ content, onChange }) {
@@ -17,6 +19,16 @@ export default function RichTextEditor({ content, onChange }) {
       }),
       Heading.configure({
         levels: [1, 2, 3],
+      }),
+      OrderedList.configure({
+        HTMLAttributes: {
+          class: "list-decimal ml-3",
+        },
+      }),
+      BulletList.configure({
+        HTMLAttributes: {
+          class: "list-disc ml-3",
+        },
       }),
       Highlight,
       Image,
